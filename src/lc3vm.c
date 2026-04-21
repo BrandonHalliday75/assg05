5c6c6495a92dc9a0b93187cc34414cb045a26d9c
Brandon Halliday <BrandonHalliday75@gmail.com>
2026-04-21 22:24:12 +0000

    Modified mem_read() & mem_write() functions to include ACV memory protection checks Modified rti() function to read PSR and RPC into locals before restoring to avoid ACV on supervisor stack

--- src/lc3vm.c
+++ src/lc3vm.c
@@ -49,6 +49,12 @@ uint16_t PC_START = 0x3000;
  */
 uint16_t mem_read(uint16_t address)
 {
+  if (is_user_mode() && (address < 0x3000 || address > 0xFDFF))
+  {
+    except(0x02);
+    return 0x0;
+  }
+
   if (address == KBDR_ADDR)
   {
     iomap[KBSR] &= 0x7FFF;
@@ -74,6 +80,12 @@ uint16_t mem_read(uint16_t address)
  */
 void mem_write(uint16_t address, uint16_t val)
 {
+  if (is_user_mode() && (address < 0x3000 || address > 0xFDFF))
+  {
+    except(0x02);
+    return;
+  }
+
   if (address == DDR_ADDR)
   {
     iomap[DSR] &= 0x7FFF;
@@ -475,11 +487,14 @@ void rti(uint16_t i)
     return;
   }
 
-  reg[PSR] = mem_read(reg[R6]);
+  uint16_t restored_psr = mem_read(reg[R6]);
   pop();
 
-  reg[RPC] = mem_read(reg[R6]);
+  uint16_t restored_rpc = mem_read(reg[R6]);
   pop();
+
+  reg[PSR] = restored_psr;
+  reg[RPC] = restored_rpc;
 
   if (is_user_mode())
   {
